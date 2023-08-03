600137e309bf8038e5c9bdb0cc6613f9c4f546c4
Your Name <you@example.com>
2023-08-03 21:58:10 +0530

    updating the Auto Push

--- main.tf
+++ main.tf
@@ -1,5 +1,25 @@
 #Create the S3 bucket
 
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucketname
+}
+
+#Bucket ownership access 
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+#making s3 bucket as public access 
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
